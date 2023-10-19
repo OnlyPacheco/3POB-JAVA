4934676ac651aab48f6cfdc611a03ab6916cea16
Guilherme Pacheco Da Silva <101291879+OnlyPacheco@users.noreply.github.com>
2023-10-19 16:18:08 -0300

    Update IncluirProdutoCarrinho.java

--- IncluirProdutoCarrinho.java
+++ IncluirProdutoCarrinho.java
@@ -2,17 +2,13 @@ import java.util.ArrayList;
 
 public class IncluirProdutoCarrinho 
 {
-    public static void incluirProdutoCarrinho(ArrayList<ItensCarrinho> carrinho, ArrayList<Produto> listaProdutos, String idProduto, int quantidade) {
-        for(Produto produto : listaProdutos) {
-            if(produto.getID().equals(idProduto)) {
-                ItensCarrinho item = new ItensCarrinho();
-                item.setID(produto);
-                item.setQuantidade(quantidade);
-                carrinho.add(item);
-                System.out.println("Produto adicionado ao carrinho com sucesso!");
-                return;
-            }
-        }
-        System.out.println("Produto não encontrado.");
+  public static void incluirProdutoCarrinho(ArrayList<ItensCarrinho> carrinho, Produto produto, int quantidade) {
+      ItensCarrinho item = new ItensCarrinho();
+      item.setID(produto); 
+      item.setQuantidade(quantidade); 
+      carrinho.add(item);
+      System.out.println("Produto adicionado ao carrinho com sucesso!");
+  }
+       
     }
-}
+
