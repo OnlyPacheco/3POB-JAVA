021a033ba27b460a0a0e4af674dfb8b23fadd1d9
Guilherme Pacheco Da Silva <101291879+OnlyPacheco@users.noreply.github.com>
2023-10-19 16:13:42 -0300

    Update ListarProdutosIncluidosCarrinho.java
    Atualização

--- ListarProdutosIncluidosCarrinho.java
+++ ListarProdutosIncluidosCarrinho.java
@@ -3,14 +3,18 @@
 import java.util.ArrayList;
 
 public class ListarProdutosIncluidosCarrinho {
-    public static void listarProdutosIncluidos(ArrayList<ItensCarrinho> carrinho) {
+    public static void listarProdutosIncluidos(ArrayList<ItensCarrinho> carrinho, Carrinho carrinhoObj) {
+  
         for (ItensCarrinho item : carrinho) {
-            System.out.println("ID do Produto: " + item.getID().getidProduto());
-            System.out.println("Nome do Produto: " + item.getID().getnome());
-            System.out.println("Valor: " + item.getID().getValor());
+            Produto produto = item.getID();
+            System.out.println("ID do Produto: " + produto.getidProduto());
+            System.out.println("Nome do Produto: " + produto.getnome());
+            System.out.println("Valor: " + produto.getValor());
             System.out.println("Quantidade: " + item.getQuantidade());
             System.out.println();
         }
+
+        double carrinhoTotal = carrinhoObj.getValorTotal();
+        System.out.println("Valor Total do Carrinho: " + carrinhoTotal);
     }
 }
- 
