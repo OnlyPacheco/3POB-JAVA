2533050e454e045b9bf76f10096b4fbdcc43c987
Guilherme Pacheco Da Silva <101291879+OnlyPacheco@users.noreply.github.com>
2023-10-19 16:18:45 -0300

    Update Carrinho.java

--- Carrinho.java
+++ Carrinho.java
@@ -2,20 +2,32 @@ import java.util.ArrayList;
 
 public class Carrinho {
     private ArrayList<ItensCarrinho> itens;
+    private double valorTotal;
 
     public Carrinho() {
         this.itens = new ArrayList<>();
+        this.valorTotal = 0.0; 
     }
 
     public void adicionarItem(ItensCarrinho item) {
         this.itens.add(item);
+        atualizarValorTotal();
     }
 
-    public double calcularValorTotal() {
+    public void atualizarCarrinho(ArrayList<ItensCarrinho> carrinho) {
+        this.itens = carrinho;
+        atualizarValorTotal();
+    }
+
+    public double getValorTotal() {
+        return this.valorTotal;
+    }
+
+    private void atualizarValorTotal() {
         double total = 0;
         for (ItensCarrinho item : this.itens) {
             total += item.getID().getValor() * item.getQuantidade();
         }
-        return total;
+        this.valorTotal = total;
     }
 }
