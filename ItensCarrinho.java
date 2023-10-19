5b22ce6e456d80618e19cd339b3639e9ab208a96
Guilherme Pacheco Da Silva <101291879+OnlyPacheco@users.noreply.github.com>
2023-10-19 16:15:14 -0300

    Update ItensCarrinho.java

--- ItensCarrinho.java
+++ ItensCarrinho.java
@@ -1,16 +1,28 @@
-// Fazer apenas com que receba o id do produto que vem da classe Produto e salvar como this.ID por exemplo ,e a quantidade do produto que deve ser inserida por uma função separada ( exemplo IncluirProdutoCarrinho ) 
+import java.util.ArrayList;
 
 import java.util.ArrayList;
 
-public class ItensCarrinho {
+public class ItensCarrinho 
+{
     private Produto ID;
     private int quantidade;
 
-    public void setID(Produto id) {
+    public void setID(Produto id) 
+  {
         this.ID = id;
     }
 
-    public void setQuantidade(int quantidade) {
+    public Produto getID() 
+  {
+        return ID;
+    }
+
+    public void setQuantidade(int quantidade)
+  {
         this.quantidade = quantidade;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
     }
 }
